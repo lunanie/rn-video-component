8a537ff33367b98e9a58c3e53a3bf58c3e8ed247
lunanie <lunanie@gmail.com>
2020-01-09 17:45:57 -0300

    audio streaming

--- video/android/app/src/main/java/com/platzivideoapp/MainActivity.java
+++ video/android/app/src/main/java/com/platzivideoapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.video;
+package com.platzivideoapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "video";
+    return "PlatziVideoApp";
   }
 }
--- video/ios/PlatziVideoAppTests/PlatziVideoAppTests.m
+++ video/ios/PlatziVideoAppTests/PlatziVideoAppTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface videoTests : XCTestCase
+@interface PlatziVideoAppTests : XCTestCase
 
 @end
 
-@implementation videoTests
+@implementation PlatziVideoAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
